539223d8f46c853899fdd5b74bb8c7a0e200e6f9
ars <ars@git.com>
2017-09-04 21:29:57 +0530

    Updated the db name

--- CatalogApp/__init__.py
+++ CatalogApp/__init__.py
@@ -23,7 +23,7 @@ APPLICATION_NAME = "Item Catalog Application"
 
 
 # Connect to Database and create database session
-engine = create_engine('postgresql://catalog:catalog@localhost:8000/itemcatalogwithusers.db')
+engine = create_engine('postgresql://catalog:catalog@localhost:80/itemcatalogwithusers')
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
